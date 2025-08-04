54ff943c772d1f66d631b3a22d40ae1a8ee1606a
하정호 <hjh4212@naver.com>
2025-08-04 14:44:18 +0900

    0524/2 수정

--- SAFFY/0524/2번.py
+++ SAFFY/0524/2번.py
@@ -5,30 +5,42 @@ for i in range(1, x+1):
     array = list(map(int, input().strip().split()))
 
     answer = 0
-    check = 0
-    tmp = array[0]
+    max_jump = 0
+    temp_dist = 0
 
     for j in range(1, len(array)):
-        if array[j] > tmp:
-            answer += array[j] - tmp
-        else:
-            check = 1
-            answer += M - (tmp - array[j])
-        tmp = array[j]
 
-    if check == 1:
-        answer -= M
+        if array[j] > array[j-1]:
+            dist1 = array[j] - array[j-1]
+        else:
+            dist1 = M - (array[j-1] - array[j])
+        answer += dist1
+
+        if j==len(array)-1:
+            continue
+        
+        if array[j+1] > array[j]:
+            dist2 = array[j+1] - array[j]
+        else:
+            dist2 = M - (array[j] - array[j+1])
+
+        if array[j+1] > array[j-1]:
+            dist3 = array[j+1] - array[j-1]
+        else:
+            dist3 = M - (array[j-1] - array[j+1])
+
+        max_jump = max(max_jump, dist1+dist2-dist3)
+
+    if array[1] > array[0]:
+        a = array[1] - array[0]
     else:
-        if array[1] > array[0]:
-            a = array[1] - array[0]
-        else:
-            a = M - array[1] + array[0]
+        a = M - array[0] + array[1]
 
-        if array[-1] > array[-2]:
-            b = array[-1] - array[-2]
-        else:
-            b = M - array[-1] + array[-2]
+    if array[-1] > array[-2]:
+        b = array[-1] - array[-2]
+    else:
+        b = M - array[-2] + array[-1]
 
-        answer -= max(a, b)
+    answer -= max(a, b, max_jump)
 
     print(f"#{i} {answer}")
