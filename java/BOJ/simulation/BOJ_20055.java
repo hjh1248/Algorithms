8df49658e1d906d0e911b76e254af10653b86ae1
hjh1248 <hjh4212@naver.com>
2026-02-19 20:06:06 +0900

    house

---
+++ java/BOJ/simulation/BOJ_20055.java
@@ -0,0 +1,20 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_20055 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int[] belt = new int[2N];
+
+
+        
+    }
+}
